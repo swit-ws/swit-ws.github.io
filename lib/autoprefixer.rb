3e1d4715133bdb14fd497a2a945d617f63418c16
sebastian.bader <sebastian.bader@kit.edu>
2018-03-13 18:37:29 +0100

    adjusting dates

--- lib/autoprefixer.rb
+++
@@ -1,5 +0,0 @@
-require 'autoprefixer-rails'
-
-Nanoc::Filter.define(:autoprefixer) do |content, params|
-  AutoprefixerRails.process content, params.dup
-end
--- lib/default.rb
+++
@@ -1,8 +0,0 @@
-include Nanoc::Helpers::LinkTo
-include Nanoc::Helpers::Rendering
-include Nanoc::Helpers::Text
-
-def main_pages
-  @items.find_all('/*.md').
-         sort_by {|p| p[:order] || Float::INFINITY }
-end
